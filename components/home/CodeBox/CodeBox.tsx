b66299bffa3f3747651e62e1b874fe78c73abd05
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-08 11:50:09 +0530

    adding 2 draggable windows

--- components/home/CodeBox/CodeBox.tsx
+++ components/home/CodeBox/CodeBox.tsx
@@ -4,7 +4,7 @@
 import React, { useState } from 'react';
 import Draggable from 'react-draggable';
 
-const CodeBox = () => {
+const CodeBox = ({ id }) => {
   const [isClosed, setIsClosed] = useState(false);
   const [currentPage, setCurrentPage] = useState('index.js');
   const [isTerminalActive, setIsTerminalActive] = useState(false);
@@ -89,114 +89,147 @@ const CodeBox = () => {
   };
 
   return (
-    <div className="h-screen flex justify-center items-center relative">
-      <Draggable handle=".handle-drag">
-        <div className="bg-gray-800 rounded-lg shadow-xl p-4 relative" style={codeEditorStyles}>
-          <div
-            className="handle-drag cursor-move bg-gray-900 px-3 py-2"
-            style={{ position: 'absolute', top: 0, left: 0, width: '100%', display: 'flex', alignItems: 'center' }}
-          >
-            <div style={{ flex: 1, display: 'flex', alignItems: 'center', gap: 10 }}>
-              <div className="flex gap-1">
-                <div className="w-2.5 h-2.5 bg-red-500 rounded-full cursor-pointer" onClick={handleCloseClick}></div>
-                <div className="w-2.5 h-2.5 bg-yellow-500 rounded-full"></div>
-                <div className="w-2.5 h-2.5 bg-green-500 rounded-full"></div>
+    <Draggable handle=".handle-drag">
+      <div className="bg-gray-800 rounded-lg shadow-xl p-4 relative" style={codeEditorStyles}>
+        <div
+          className="handle-drag cursor-move bg-gray-900 px-3 py-2"
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', display: 'flex', alignItems: 'center' }}
+        >
+          <div style={{ flex: 1, display: 'flex', alignItems: 'center', gap: 10 }}>
+            <div className="flex gap-1">
+              <div className="w-2.5 h-2.5 bg-red-500 rounded-full cursor-pointer" onClick={handleCloseClick}></div>
+              <div className="w-2.5 h-2.5 bg-yellow-500 rounded-full"></div>
+              <div className="w-2.5 h-2.5 bg-green-500 rounded-full"></div>
+            </div>
+            <div className="flex gap-2">
+              <div
+                onClick={() => changePage('index.js')}
+                style={{
+                  backgroundColor: currentPage === 'index.js' ? 'rgb(66, 66, 66)' : 'transparent',
+                  padding: '3px 6px',
+                  borderRadius: '4px',
+                  display: 'flex',
+                  cursor: 'pointer',
+                }}
+              >
+                <p className="m-0" style={{ color: 'white', fontSize: '11px', alignSelf: 'center' }}>
+                  index.js ×
+                </p>
               </div>
-              <div className="flex gap-2">
-                <div
-                  onClick={() => changePage('index.js')}
-                  style={{
-                    backgroundColor: currentPage === 'index.js' ? 'rgb(66, 66, 66)' : 'transparent',
-                    padding: '3px 6px',
-                    borderRadius: '4px',
-                    display: 'flex',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <p className="m-0" style={{ color: 'white', fontSize: '11px', alignSelf: 'center' }}>
-                    index.js ×
-                  </p>
-                </div>
-                <div
-                  onClick={() => changePage('aboutme.txt')}
-                  style={{
-                    backgroundColor: currentPage === 'aboutme.txt' ? 'rgb(66, 66, 66)' : 'transparent',
-                    padding: '3px 6px',
-                    borderRadius: '4px',
-                    display: 'flex',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <p className="m-0" style={{ color: 'white', fontSize: '11px', alignSelf: 'center' }}>
-                    aboutme.txt
-                  </p>
-                </div>
+              <div
+                onClick={() => changePage('aboutme.txt')}
+                style={{
+                  backgroundColor: currentPage === 'aboutme.txt' ? 'rgb(66, 66, 66)' : 'transparent',
+                  padding: '3px 6px',
+                  borderRadius: '4px',
+                  display: 'flex',
+                  cursor: 'pointer',
+                }}
+              >
+                <p className="m-0" style={{ color: 'white', fontSize: '11px', alignSelf: 'center' }}>
+                  aboutme.txt x
+                </p>
+              </div>
+              <div
+                onClick={() => changePage('main.py')}
+                style={{
+                  backgroundColor: currentPage === 'main.py' ? 'rgb(66, 66, 66)' : 'transparent',
+                  padding: '3px 6px',
+                  borderRadius: '4px',
+                  display: 'flex',
+                  cursor: 'pointer',
+                }}
+              >
+                <p className="m-0" style={{ color: 'white', fontSize: '11px', alignSelf: 'center' }}>
+                  main.py x
+                </p>
               </div>
             </div>
           </div>
+        </div>
 
-          <div className="p-4 text-white" style={contentStyles}>
-            {currentPage === 'index.js' && (
-              <div>
-                <pre>
-                  <code>
-                    const details = &#123;
-                    <br />
-                    &nbsp;name: &apos;MockOS&apos;,
-                    &nbsp;age: &apos;Under Construction&apos;
-                    <br />
-                    &#125;
-                  </code>
-                </pre>
+        <div className="p-4 text-white" style={contentStyles}>
+          {currentPage === 'index.js' && (
+            <div>
+              <pre>
+                <code>
+                  const details = &#123;
+                  <br />
+                  &nbsp;name: &apos;MockOS&apos;,
+                  &nbsp;age: &apos;Under Construction&apos;
+                  <br />
+                  &#125;
+                </code>
+              </pre>
 
-                <div className="absolute bottom-0 w-full" style={terminalStyles} onClick={handleTerminalClick}>
-                  <div style={terminalContainerStyles}>
-                    {terminalHistory.map((item, index) => (
-                      <div key={index}>
-                        <div className="text-green-600">
-                          {isTerminalActive ? 'arnab@portfolio ~ ' : ''}
-                          {isTerminalActive ? '$' : ''}
-                          &nbsp;
-                          <span className="text-white">{item.input}</span>
-                        </div>
-                        <div>{item.output}</div>
+              <div className="absolute bottom-0 w-full" style={terminalStyles} onClick={handleTerminalClick}>
+                <div style={terminalContainerStyles}>
+                  {terminalHistory.map((item, index) => (
+                    <div key={index}>
+                      <div className="text-green-600">
+                        {isTerminalActive ? 'arnab@portfolio ~ ' : ''}
+                        {isTerminalActive ? '$' : ''}
+                        &nbsp;
+                        <span className="text-white">{item.input}</span>
                       </div>
-                    ))}
-                    <div style={{ display: 'flex', flexDirection: 'row' }}>
-                      <p className="text-green-600">arnab@portfolio ~ $</p>
-                      &nbsp;
-                      <div>
-                        <input
-                          style={inputStyles}
-                          value={terminalInput}
-                          onChange={handleTerminalInputChange}
-                          onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                              handleRunCommand();
-                            }
-                          }}
-                          autoFocus
-                        />
-                      </div>
+                      <div>{item.output}</div>
+                    </div>
+                  ))}
+                  <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <p className="text-green-600">arnab@portfolio ~ $</p>
+                    &nbsp;
+                    <div>
+                      <input
+                        style={inputStyles}
+                        value={terminalInput}
+                        onChange={handleTerminalInputChange}
+                        onKeyDown={(e) => {
+                          if (e.key === 'Enter') {
+                            handleRunCommand();
+                          }
+                        }}
+                        autoFocus
+                      />
                     </div>
                   </div>
                 </div>
               </div>
-            )}
-            {currentPage === 'aboutme.txt' && (
-              <div>
-                <p>Arnab this side.</p>
-                <p>How are you all!!!</p>
-                <hr />
-                <p>We are doing great</p>
-                <p>Excited to contribute to Hacktoberfest.</p>
-              </div>
-            )}
-          </div>
+            </div>
+          )}
+          {currentPage === 'aboutme.txt' && (
+            <div>
+              <p>Arnab this side.</p>
+              <p>How are you all!!!</p>
+              <hr />
+              <p>We are doing great</p>
+              <p>Excited to contribute to Hacktoberfest.</p>
+            </div>
+          )}
+          {currentPage === 'main.py' && (
+            <div>
+              <p>import os</p>
+              <p>import numpy as np</p>
+              <p></p>
+              <p>#Program to print</p>
+              <p>print("Hello World")</p>
+            </div>
+          )}
         </div>
-      </Draggable>
+      </div>
+    </Draggable>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center relative">
+      {/* First draggable CodeBox */}
+      <CodeBox id="codebox1" />
+
+      {/* Second draggable CodeBox */}
+      <CodeBox id="codebox2" />
     </div>
   );
 };
 
-export default CodeBox;
+export default App;
